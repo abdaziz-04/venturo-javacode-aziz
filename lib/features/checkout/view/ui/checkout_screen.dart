615907eb0e4d8bff610a9bb911e8269875b13806
abdaziz-04 <abdaziz1713@gmail.com>
2025-02-21 01:03:03 +0700

    fix: minor changes

--- lib/features/checkout/view/ui/checkout_screen.dart
+++ lib/features/checkout/view/ui/checkout_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_null_comparison, invalid_use_of_protected_member
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
